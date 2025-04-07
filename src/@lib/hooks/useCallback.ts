d2a66c2df1ebcf7a71554560914f02ef9fd19961
unseoJang <qqbck123@naver.com>
2025-04-07 22:38:31 +0900

    [useMemo] 제작 완료

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import type { DependencyList } from "react";
 import { useMemo } from "./useMemo";
 
@@ -7,7 +7,7 @@ export function useCallback<T extends Function>(
   _deps: DependencyList,
 ) {
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  const memoizedCallback = useMemo(() => factory, [..._deps]);
+  const memoizedCallback = useMemo(() => factory, _deps);
 
   return memoizedCallback as T;
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
 import { useRef } from "react";
